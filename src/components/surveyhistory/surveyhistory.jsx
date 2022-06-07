23fe7506ee4e7798fe958b7142761484d379ca5c
ashrafali-v <ashrafalifrk@gmail.com>
2022-06-07 16:00:28 +0800

    contactId by url update

--- src/components/surveyhistory/surveyhistory.jsx
+++ src/components/surveyhistory/surveyhistory.jsx
@@ -2,66 +2,74 @@ import React, { useState,useEffect } from 'react';
 import './surveyhistory.scss';
 import 'react-toastify/dist/ReactToastify.css';
 import SurveyTable from './SurveyHistoryTable/surveyhistorytable';
+import { useSearchParams } from "react-router-dom"
 
 function SurveyHistory() {
     const urlBase = process.env.REACT_APP_BASE_URL;
-    const [surveyData, setSurveyData] = useState('');
+    const [surveyData, setSurveyData] = useState([]);
     const [searchkey, setSearchKey] = useState('');
+    const [isSearch, setIsSearch] = useState(false);
     const [searchType, setSearchType] = useState('contactId');
-    const url = `${urlBase}getSurveyHistory?${searchType}=${searchkey}`;
-    const getSurveyHistory = async () => {
+    let [searchParams, setSearchParams] = useSearchParams('')
+
+
+
+    const getSurveyHistory = async (searchkey) => {
+        setIsSearch(true);
+         const url = `${urlBase}getSurveyHistory?${searchType}=${searchkey}`;
         try {
             const response = await fetch(url);
             const json = await response.json();
             console.log(json);
-            //let data = json.message;
-            let data = [
-          {
-             "id": "6c218e41-3af9-439f-9fee-d657a79e1e57",
-            "companyCode": "PPAY",
-            "companyName": "PlacePay",
-            "surveyName": "PlacePay CES",
-            "surveyType": "CES",
-            "phone": "7862471779",
-            "contactId": "12344455ffffgfg",
-            "q1Response": "5",
-            "q2Response": "5",
-            "q3Response": "5",
-            "q4Response": "5",
-            "q5Response": "5",
-            "date": "12/31/2021"
-          },
-          {
-              "id": "6c238e41-3af9-439f-9fee-d657a79e1e37",
-            "companyCode": "PPAY",
-            "companyName": "PlacePay",
-            "surveyName": "PlacePay CSAT",
-            "surveyType": "CSAT",
-            "phone": "7862471779",
-            "contactId": "22344455ffffgfg",
-            "q1Response": "5",
-            "q2Response": "5",
-            "q3Response": "5",
-            "q4Response": "5",
-            "q5Response": "5",
-            "date": "12/31/2021"
-          } 
-            ]
-            const surveyProcessed = data.map(surveyData => ({id: surveyData.id,Survey:  surveyData.surveyName, 
+            let data = json.message;
+        //     let data = [
+        //   {
+        //      "id": "1",
+        //     "companyCode": "PPAY",
+        //     "companyName": "PlacePay",
+        //     "surveyName": "PlacePay CES",
+        //     "surveyType": "CES",
+        //     "phone": "7862471779",
+        //     "contactId": "12344455ffffgfg",
+        //     "q1Response": "5",
+        //     "q2Response": "5",
+        //     "q3Response": "5",
+        //     "q4Response": "5",
+        //     "q5Response": "5",
+        //     "date": "12/31/2021"
+        //   },
+        //   {
+        //       "id": "2",
+        //     "companyCode": "PPAY",
+        //     "companyName": "PlacePay",
+        //     "surveyName": "PlacePay CSAT",
+        //     "surveyType": "CSAT",
+        //     "phone": "7862471779",
+        //     "contactId": "22344455ffffgfg",
+        //     "q1Response": "5",
+        //     "q2Response": "5",
+        //     "q3Response": "5",
+        //     "q4Response": "5",
+        //     "q5Response": "5",
+        //     "date": "12/31/2021"
+        //   } 
+        //     ]
+            const surveyProcessed = data.map((surveyData,index) => ({id: index,Survey:  surveyData.surveyName, 
             CompanyName: surveyData.companyName,
             survey: surveyData.surveyName,
             surveyType: surveyData.surveyType,
-            q1Response: surveyData.q1Response,
-            q2Response: surveyData.q2Response,
-            q3Response: surveyData.q3Response,
-            q4Response: surveyData.q4Response,
-            q5Response: surveyData.q5Response,
-            Created:new Date(surveyData.date).toLocaleDateString("en-US"),
+            Q1: surveyData.Q1,
+            Q2: surveyData.Q2,
+            Q3: surveyData.Q3,
+            Q4: surveyData.Q4,
+            Q5: surveyData.Q5,
+            Created:surveyData.timeStamp,
           }));
             setSurveyData(surveyProcessed);
             console.log(surveyData);
         } catch (error) {
             console.log("error", error);
+            setSurveyData([]);
         }
     };
     const handleChange = (event)=>{
@@ -74,7 +82,14 @@ function SurveyHistory() {
     }
     useEffect(() => {
       //fetchData();
-    }, []);
+        const cId = searchParams.get("contactId")
+        if(cId){
+            setSearchKey(cId);
+            setTimeout(function(){
+             getSurveyHistory(searchkey);
+            },500)
+        }
+    }, [searchkey]);
     let surveyList = <SurveyTable data={surveyData}/>;
     return (
         <div className='content-container surveys'>
@@ -82,14 +97,13 @@ function SurveyHistory() {
             <div>
                 <label htmlFor="search">Search</label><br></br>
                 <input name="search" onChange={(e) => handleChange(e)} type="text" placeholder='Contactid/Phone Number' />
-                <button className="search-button" onClick={getSurveyHistory}>Search</button>
-
+                <button className="search-button" onClick={()=>getSurveyHistory(searchkey)}>Search</button>
                 <select class="type-filter" name="type" id="type" onChange={(e) => handleTypeChange(e)}>
                     <option value="contactId">Contact Id</option>
-                    <option value="contactNumber">Contact Number</option>
+                    <option value="contactNumber">Phone Number</option>
                 </select>
             </div>
-                {surveyList}
+            {surveyData.length > 0? surveyList:isSearch?<span>No data found</span>:<></>}
         </div>
     )
 }
